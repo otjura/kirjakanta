6af300fd7d79f0ba1e15a032d8eb919b4711c688
otjura <ojrajala@gmail.com>
2016-06-11 20:26:07 +0300

    connecting and commanding sqlite via clojure works. can check for existing db file and initialize new one if needed. added db-files to gitignore.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject kirjakanta "0.1.0-SNAPSHOT"
-  :description "Simple book database"
+  :description "Yksinkertainen kirjatietokanta"
   :url "https://github.com/otjura/kirjakanta"
   :license {:name "The MIT License (MIT)"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/java.jdbc "0.6.1"]]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]]
   :main ^:skip-aot kirjakanta.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kirjakanta/core.clj
+++ src/kirjakanta/core.clj
@@ -1,15 +1,25 @@
 (ns kirjakanta.core 
   (:gen-class)
   (:import (javax.sql ConnectionEvent RowSetEvent StatementEvent))
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc]))
 
-(def database {:classname "org.sqlite.JDBC"
-               :subprotocol "sqlite"
-               :subname ".db"})
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname "kirjakanta.db"})
+
+(def table (jdbc/create-table-ddl :books [[:id :integer :primary :key :autoincrement]
+                                          [:title :text]
+                                          [:author :text]
+                                          [:year :text]]))
 
 (defstruct book :title :author :year)
 
 (defn -main [& args]
-  ())
+  (initialize-database))
 
-
+(defn initialize-database []
+  (if-not (first (map #(.exists (io/as-file %)) '("./kirjakanta.db")))
+    (do (jdbc/execute! db [table])
+        (println "Luotiin uusi kirjatietokanta."))
+    (do (println "Tietokanta on olemassa."))))
