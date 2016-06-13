a5dae038e9c383ede9266fd7c3a2d8612bf11916
otjura <ojrajala@gmail.com>
2016-06-13 20:09:01 +0300

    basic database handling

--- README.md
+++ README.md
@@ -27,19 +27,4 @@ FIXME: listing of options this app accepts.
 
 ## Examples
 
-...
 
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
-
-## License
-
-Copyright © 2016 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- src/kirjakanta/core.clj
+++ src/kirjakanta/core.clj
@@ -1,17 +1,26 @@
 (ns kirjakanta.core 
   (:gen-class)
-  (:import (javax.sql ConnectionEvent RowSetEvent StatementEvent))
+  (:import (javax.sql ConnectionEvent RowSetEvent StatementEvent)
+           (javax.swing JFrame JPanel JLabel JButton)
   (:require [clojure.java.io :as io]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as sql]))
 
 (def db {:classname "org.sqlite.JDBC"
          :subprotocol "sqlite"
          :subname "kirjakanta.db"})
 
-(def table (jdbc/create-table-ddl :books [[:id :integer :primary :key :autoincrement]
-                                          [:title :text]
-                                          [:author :text]
-                                          [:year :text]]))
+(def table (sql/create-table-ddl :books [[:id :integer :primary :key :autoincrement]
+                                         [:title :text]
+                                         [:author :text]
+                                         [:year :text]]))
+
+(def query-all "select * from books")
+
+(def query-titles "select title from books")
+
+(def query-authors "select author from books")
+
+(def query-years "select year from books")
 
 (defstruct book :title :author :year)
 
@@ -20,6 +29,21 @@
 
 (defn initialize-database []
   (if-not (first (map #(.exists (io/as-file %)) '("./kirjakanta.db")))
-    (do (jdbc/execute! db [table])
+    (do (sql/execute! db [table])
         (println "Luotiin uusi kirjatietokanta."))
     (do (println "Tietokanta on olemassa."))))
+
+(defn add-entry
+  ([title author year]
+   (sql/insert! db :books {:title title :author author :year year}))
+  ([title author]
+   (sql/insert! db :books {:title title :author author}))
+  ([title]
+   (sql/insert! db :books {:title title})))
+
+(defn edit-entry [id what new-value]
+  (case what
+    "title" (sql/update! db :books {:id id :title new-value})
+    "author" (sql/update! db :books {:id id :author new-value})
+    "year" (sql/update! db :books {:id id :year new-value})
+    "Ei ole olemassa."))
